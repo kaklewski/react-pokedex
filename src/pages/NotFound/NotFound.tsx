a6195a24884d6248c461e8e6404b6803714967ba
Oskar Kąklewski <oskarkaklewski@gmail.com>
2025-11-10 18:24:29 +0100

    feat: Add 404 page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,23 @@
+import { Button, Container, Stack, Text, Title } from '@mantine/core';
+import { IconArrowLeft } from '@tabler/icons-react';
+import { Link } from '@tanstack/react-router';
+
+export function NotFound() {
+    return (
+        <Container>
+            <Stack align="center" justify="center" style={{ height: '80vh' }}>
+                <Title order={1}>404 - Page Not Found</Title>
+                <Text>Check the URL or return to the home page.</Text>
+                <Button
+                    leftSection={<IconArrowLeft size={14} />}
+                    variant="light"
+                    color="red"
+                    component={Link}
+                    to="/"
+                >
+                    Go to Home
+                </Button>
+            </Stack>
+        </Container>
+    );
+}
