6a44a1f38676062e300dea15d268b94777f0d351
Oskar Kąklewski <oskarkaklewski@gmail.com>
2025-11-10 17:56:42 +0100

    style: Add motion hover animation

--- src/pages/Home/components/PokemonCard.tsx
+++ src/pages/Home/components/PokemonCard.tsx
@@ -1,5 +1,6 @@
 import { Card, Image, Text } from '@mantine/core';
 import { Link } from '@tanstack/react-router';
+import { motion } from 'framer-motion';
 import { capitalizeFirstLetter, formatPokemonId } from '../../../utils/helpers';
 import classes from './PokemonCard.module.css';
 
@@ -13,26 +14,31 @@ export default function PokemonCard({ id, name }: PokemonCardProps) {
     const nameCapitalized = capitalizeFirstLetter(name);
 
     return (
-        <Card
-            component={Link}
-            to={`/pokemon/${name}`}
-            shadow="sm"
-            radius="md"
-            withBorder
-            className={classes.card}
+        <motion.div
+            whileHover={{ scale: 1.03 }}
+            whileTap={{ scale: 0.98 }}
+            transition={{ type: 'spring', stiffness: 300, damping: 15 }}
         >
-            <Card.Section className={classes.image} p={8} withBorder>
-                <Image
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
-                    alt={name}
-                />
-            </Card.Section>
+            <Card
+                component={Link}
+                to={`/pokemon/${name}`}
+                shadow="sm"
+                radius="md"
+                withBorder
+            >
+                <Card.Section className={classes.image} p={8} withBorder>
+                    <Image
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+                        alt={name}
+                    />
+                </Card.Section>
 
-            <Text size="sm" c="dimmed" mt={8} mb={4}>
-                {idString}
-            </Text>
+                <Text size="sm" c="dimmed" mt={8} mb={4}>
+                    {idString}
+                </Text>
 
-            <Text size="lg">{nameCapitalized}</Text>
-        </Card>
+                <Text size="lg">{nameCapitalized}</Text>
+            </Card>
+        </motion.div>
     );
 }
