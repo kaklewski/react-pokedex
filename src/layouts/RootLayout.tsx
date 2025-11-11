ea352bc5841f4f93bcc71b7d4ba9f9739317b169
Oskar Kąklewski <oskarkaklewski@gmail.com>
2025-11-11 22:24:08 +0100

    feat: Add page transition animation

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from '@mantine/core';
 import { useRouterState } from '@tanstack/react-router';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect } from 'react';
 import AppShellLayout from './AppShellLayout';
 import Footer from './Footer';
@@ -11,13 +12,24 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
     const routerState = useRouterState();
+
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [routerState.location.pathname]);
 
     return (
         <Stack className={classes.rootLayout}>
-            <AppShellLayout>{children}</AppShellLayout>
+            <AnimatePresence mode="wait">
+                <motion.div
+                    key={routerState.location.pathname}
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.3, ease: [0.4, 0, 0.2, 1] }}
+                >
+                    <AppShellLayout>{children}</AppShellLayout>
+                </motion.div>
+            </AnimatePresence>
+
             <Footer />
         </Stack>
     );
