681c062e543a5a31084e1c05a12a80ecb2a48c9c
Oskar Kąklewski <oskarkaklewski@gmail.com>
2025-11-11 17:41:09 +0100

    style: Change 404 text styling

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,21 +1,18 @@
 import { Button, Container, Stack, Text, Title } from '@mantine/core';
-import { IconArrowLeft } from '@tabler/icons-react';
+import { IconError404 } from '@tabler/icons-react';
 import { Link } from '@tanstack/react-router';
 
 export default function NotFound() {
     return (
         <Container>
             <Stack align="center" justify="center" style={{ height: '80vh' }}>
-                <Title order={1}>404 - Page Not Found</Title>
-                <Text>Check the URL or return to the home page.</Text>
-                <Button
-                    leftSection={<IconArrowLeft size={14} />}
-                    variant="light"
-                    color="red"
-                    component={Link}
-                    to="/"
-                >
-                    Go to Home
+                <IconError404 size={64} color="var(--mantine-color-red-6)" />
+                <Title order={3}>Page not found</Title>
+                <Text c="dimmed" ta="center">
+                    Check the URL or return to the home page.
+                </Text>
+                <Button variant="light" color="red" component={Link} to="/">
+                    Go to home
                 </Button>
             </Stack>
         </Container>
