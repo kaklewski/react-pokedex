4166ba83e8293f8531f364deb53d8161e9c2475d
Oskar Kąklewski <oskarkaklewski@gmail.com>
2025-11-30 14:32:55 +0100

    fix: Polyfill matchMedia for better test compatibility

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom';
+
+if (!window.matchMedia) {
+    window.matchMedia = (query: string) => {
+        return {
+            matches: false,
+            media: query,
+            onchange: null,
+            addListener: () => {},
+            removeListener: () => {},
+            addEventListener: () => {},
+            removeEventListener: () => {},
+            dispatchEvent: () => false,
+        };
+    };
+}
