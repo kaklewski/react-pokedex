4dffa38852c6f02eb7123fdcd5519fa9b9ff6db9
Oskar Kąklewski <oskarkaklewski@gmail.com>
2025-12-01 18:51:19 +0100

    chore: Remove unused dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createRouter, RouterProvider } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { routeTree } from './routeTree.gen';
 
 const router = createRouter({ routeTree });
@@ -8,7 +7,6 @@ export default function App() {
     return (
         <>
             <RouterProvider router={router} />
-            <TanStackRouterDevtools router={router} />
         </>
     );
 }
